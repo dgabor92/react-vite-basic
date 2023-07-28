1e7dc3b85c8ca186660d8be380595c3d1674201a
Gábor <deutschgabor92@gmail.com>
2023-07-28 13:36:14 +0200

    routes update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
-import Signup from "./pages/Signup";
-import Dashboard from "./components/Dashboard";
-
-const queryClient = new QueryClient();
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RenderRouter from "./routes";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <RenderRouter />
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/index.tsx
@@ -0,0 +1,33 @@
+import React, { lazy, FC } from "react";
+import { useRoutes, RouteObject } from "react-router-dom";
+import Login from "../pages/Login";
+import Welcome from "../pages/Welcome";
+import Signup from "../pages/Signup";
+
+const NotFound = lazy(() => import("../pages/404"));
+
+const routeList: RouteObject[] = [
+  {
+    path: "/",
+    element: <Welcome />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "signup",
+    element: <Signup />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+];
+
+const RenderRouter: FC = () => {
+  const element = useRoutes(routeList);
+  return element;
+};
+
+export default RenderRouter;
