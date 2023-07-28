32c83722cf5f53d8c71f213904b5ce8cf488abf7
Gábor <deutschgabor92@gmail.com>
2023-07-28 13:22:40 +0200

    axios update

--- src/config/http.tsx
+++ src/config/http.tsx
@@ -1,37 +1,83 @@
-import axios, { AxiosInstance } from "axios";
+import Axios, { AxiosInstance } from "axios";
+import { notification } from "antd";
+import { createContext, useContext } from "react";
 
-const axiosInstance: AxiosInstance = axios.create({
-  baseURL:
-    import.meta.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
-  // headers: {
-  //   Origin: "http://127.0.0.1:5173",
-  // },
-  // withCredentials: true,
+const axios: AxiosInstance = Axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:8000/api",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    } else {
-      config.headers.Authorization = "";
-    }
-    return config;
-  },
-  (error) => {
-    console.log(error, "error");
-    return Promise.reject(error);
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    config.headers.Authorization = "";
   }
-);
+  return config;
+});
 
-axiosInstance.interceptors.response.use(
+axios.interceptors.response.use(
   (response) => {
-    return response;
+    const data = response.data;
+    console.log("response", response);
+    if (response.status === 200) {
+      return data;
+    }
+
+    notification.error({
+      message: `Error ${response.status}: ${response.statusText}`,
+      description: data || response.statusText || "Error",
+    });
+
+    if (response.status === 401) {
+      window.location.href = "/login";
+    }
+    return Promise.reject(new Error(response.statusText || "Error"));
   },
   (error) => {
+    console.log("error:", error, error.response);
+    if (error.response && error.response.status) {
+      switch (error.response.status) {
+        case 401:
+          window.location.href = "/login";
+          break;
+        case 403:
+          window.location.href = "/login";
+          break;
+        case 404:
+          notification.error({
+            message: `Error ${error.response.status}: ${error.response.statusText}`,
+            description: error.response.data?.msg || "Error",
+          });
+          break;
+        default:
+          notification.error({
+            message: `Error ${error.response.status}: ${error.response.statusText}`,
+            description: error.response.data?.msg || "Error",
+          });
+      }
+    }
     return Promise.reject(error);
   }
 );
 
-export default axiosInstance;
+export const AxiosContext = createContext<AxiosInstance>(
+  new Proxy(axios, {
+    apply: () => {
+      throw new Error("You must wrap you component in an AxiosProvider");
+    },
+    get: () => {
+      throw new Error("You must wrap you component in an AxiosProvider");
+    },
+  })
+);
+
+export const useAxios = () => {
+  return useContext(AxiosContext);
+};
+
+export default axios;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.css'
+import React, { Suspense, useMemo } from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import axios, { AxiosContext } from "./config/http.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+import App from "./App.tsx";
+import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      refetchInterval: false,
+    },
+  },
+});
+
+const AxiosProvider = ({ children }: React.PropsWithChildren<unknown>) => {
+  const axiosValue = useMemo(() => {
+    return axios;
+  }, []);
+
+  return (
+    <AxiosContext.Provider value={axiosValue}>{children}</AxiosContext.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <AxiosProvider>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </AxiosProvider>
+);
